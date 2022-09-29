068c474a73622630352a162f4d6b11afb488b1ce
DanielLima <danielsuporteti@outlook.com.br>
2022-09-29 09:15:16 -0300

    primeiro comentario

---
+++ Conceitos e Fundamentos/Modelo Anêmico/Anemico/ModeloAnemico.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Anemico
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public string ClienteId { get; set; }
+        public decimal Desconto { get; set; }
+        public DateTime? DataPagamento { get; set; }
+        public List<ItemPedido> Itens { get; set; }
+    }
+
+    public class ItemPedido
+    {
+        //codigo...
+    }
+
+    public class PedidoService
+    {
+        public void AddItem(int id, ItemPedido item)
+        {
+            //codigo...
+        }
+        public void DescontoPedido(int id, decimal desconto)
+        {
+            //codigo...
+        }
+        public void PagamentoPedido(int id)
+        {
+            //codigo...
+        }
+    }
+    /// <summary>
+    /// No arquivo ModeloRico.cs fiz as implementações de um modelo rico, 
+    /// onde deixamos uma classe mais limpa e mais segura de sofres mudanças
+    /// </summary>
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+
+    }
+}
---
+++ Conceitos e Fundamentos/Modelo Anêmico/Anemico/ModeloRico.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Anemico.Rico
+{
+    public class Cliente
+    {
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public string Endereco { get; private set; }
+
+        public Cliente(int id, string nome, string endereco)
+        {
+            Validar(id, nome, endereco);
+            Id = id;
+            Nome = nome;
+            Endereco = endereco;
+        }
+
+        public void Update(int id, string nome, string endereco)
+        {
+            Validar(id, nome, endereco);
+            Id = id;
+            Nome = nome;
+            Endereco = endereco;
+        }
+
+        private void Validar(int id, string nome, string endereco)
+        {
+            if (id < 0)
+                throw new InvalidOperationException("O Id tem que ser maior que 0");
+
+            if (string.IsNullOrEmpty(nome) || string.IsNullOrEmpty(endereco))
+                throw new InvalidOperationException("O nome e endereço é requirido");
+
+            if (nome.Length < 3)
+                throw new ArgumentException("O nome deve ter no mínimo 3 caracteres");
+
+            if (nome.Length > 100)
+                throw new ArgumentException("O nome excedeu 100 caracteres");
+        }
+
+    }
+}
